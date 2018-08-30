b3c7bb37e4ae71796b4b2927a5654b04b209c0a8
lidulibai <499877253@qq.com>
2018-08-30 23:04:55 +0800

    1

--- getinfo.py
+++ getinfo.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from requests.cookies import RequestCookieJar
 import lxml
 
-headerdata = {
+postderdata = {
     "geetest_challenge":   "a1d0c6e83f027327d8461063f4ac58a692",
     "geetest_seccode": "be6bbff39c53eab75a68af2347b57fd7|jordan",
     "geetest_validate":    "be6bbff39c53eab75a68af2347b57fd7",
@@ -28,17 +27,18 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"
 }
 
-cookies_jar = RequestCookieJar()
-cookies_jar.set("__jsl_clearance", "1535634953.458|0|cdSFaASwJvSt+sRwc6WrZMfwxUg=")
-cookies_jar.set("__jsluid", "e5ae9c0dff28dc050d15f2676609698c")
-cookies_jar.set("CNZZDATA1261033118", "290523213-1535351681-http%3A%2F%2Fwww.gsxt.gov.cn%2F|1535632575")
-cookies_jar.set("JSESSIONID", "31CCD2A85CC7269A62ABD3EF85A94BE4-n2:4")
-cookies_jar.set("SECTOKEN", "7097534261986329306")
-cookies_jar.set("tlb_cookie", "S172.16.12.68")
-cookies_jar.set("UM_distinctid", "165835499288e-03d2b6d633fa3a-4c312b7b-1fa400-165835499291e6")
+cookies_jar = {
+    "__jsl_clearance": "1535634953.458|0|cdSFaASwJvSt+sRwc6WrZMfwxUg=",
+    "__jsluid": "e5ae9c0dff28dc050d15f2676609698c",
+    "CNZZDATA1261033118": "290523213-1535351681-http%3A%2F%2Fwww.gsxt.gov.cn%2F|1535632575",
+    "JSESSIONID": "31CCD2A85CC7269A62ABD3EF85A94BE4-n2:4",
+    "SECTOKEN": "7097534261986329306",
+    "tlb_cookie": "S172.16.12.68",
+    "UM_distinctid": "165835499288e-03d2b6d633fa3a-4c312b7b-1fa400-165835499291e6"
+}
 
 url = 'http://www.gsxt.gov.cn/corp-query-search-1.html'
-page = requests.post(url, data = headerdata, cookies = cookies_jar)
+page = requests.post(url, data = postdata, cookies = cookies_jar)
 html = page.text
 print(html)
 #  selector = etree.HTML(html)
